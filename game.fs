501fec763ab8b40f1803628a5ebcc0e6c7ff4ff7
Vestue <61783376+Vestue@users.noreply.github.com>
2023-02-23 22:46:10 +0100

    Remove coordinate from being in tile

--- game.fs
+++ game.fs
@@ -1,4 +1,5 @@
 ﻿namespace DVA229_Lab4_AvaloniaElmish
+
 open System
 open System.Linq.Expressions
 open Avalonia
@@ -12,138 +13,114 @@ module Game =
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
     open Microsoft.FSharp.Collections
-    
-     type Cell =
-          Dead 
-          |Alive
-     
-     type Position = {
-         X : int
-         Y : int
-     }
-     type UserAction =
-         | ChangeCellState of Position
-         | Start
-         | Stop
-         | Save
-         | Load
-         | Reset
-     type Tile = {
-         Cell : Cell
-         Coordinate : Position
-     } 
-     type State = Tile[,]
-     let cellToString (state: State) (coords: UserAction)  =
-         match coords with
-         | ChangeCellState pos ->
-             match state[pos.X,pos.Y].Cell with
-                | Dead -> " "
-                | Alive -> "#"
-             
-                
-            
-                
-    
-        
-    let isCellAlive (cell : Cell) =
+
+    type Cell =
+        | Dead
+        | Alive
+
+    type Position = { X: int; Y: int }
+
+    type UserAction =
+        | ChangeCellState of Position
+        | Start
+        | Stop
+        | Save
+        | Load
+        | Reset
+
+    type Tile = { Cell: Cell; Coordinate: Position }
+    type State = Cell[,]
+
+    let cellToString (state: State) (coords: UserAction) =
+        match coords with
+        | ChangeCellState pos ->
+            match state[pos.X, pos.Y] with
+            | Dead -> " "
+            | Alive -> "#"
+        | _ -> ""
+
+
+    let isCellAlive (cell: Cell) =
         match cell with
-        | Dead  -> false
+        | Dead -> false
         | Alive -> true
 
     let gridHeight = 16
     let gridWidth = 16
-   
-    let init : State = Array2D.init 16 16 (fun i j -> {Cell = Dead; Coordinate = {X = j; Y = i}})
-    
-    let flipCellState (coordinates : Position) (state: State) : State =
-       let newState : Tile[,] =
-           Array2D.init gridHeight gridWidth
-            (fun x y ->
-               if x = coordinates.X && y = coordinates.Y then
-                   match state[coordinates.X, coordinates.Y].Cell with
-                    | Alive -> {Cell = Dead; Coordinate = {X = coordinates.X; Y = coordinates.Y}}
-                    | Dead -> {Cell = Alive; Coordinate = {X = coordinates.X; Y = coordinates.Y}} 
-               else state[x, y])
-       newState
-      
-           
-           
-       
-    let rec update (msg: UserAction) (state : State) : State=
-         match msg with
-         | Start ->  state
-         | Stop -> state
-         | Reset -> init
-         | ChangeCellState pos -> flipCellState pos state
-         
-           
-         
-         
-    let view (state : State) dispatch =
+
+    let init: State = Array2D.create gridWidth gridHeight Cell.Dead
+
+    let flipCellState (coordinates: Position) (state: State) : State =
+        let newState: Cell[,] =
+            Array2D.init gridHeight gridWidth (fun x y ->
+                if x = coordinates.X && y = coordinates.Y then
+                    match state[coordinates.X, coordinates.Y] with
+                    | Alive -> Cell.Dead
+                    | Dead -> Cell.Alive
+                else
+                    state[x, y])
+
+        newState
+
+    let rec update (msg: UserAction) (state: State) : State =
+        match msg with
+        | Start -> state
+        | Stop -> state
+        | Reset -> init
+        | ChangeCellState pos -> flipCellState pos state
+        | _ -> state
+
+    let view (state: State) dispatch =
         printfn $"{state}"
         let squareWidth = 25.0
         let squareHeight = 25.0
         let buttonsInColumn = 16.0
-        
-        
-        
-        let createButton (cellCoordinates : UserAction) =
-            Button.create [
-                Button.width(squareWidth)
-                Button.height(squareHeight)
-                Button.content(cellToString state cellCoordinates)
-                Button.onClick(fun _ -> dispatch cellCoordinates)
-                
-            ]
-            
-        DockPanel.create [
-            DockPanel.children [
-                WrapPanel.create [               
-                        WrapPanel.horizontalAlignment HorizontalAlignment.Center
-                        WrapPanel.verticalAlignment VerticalAlignment.Center
-                        WrapPanel.dock Dock.Bottom
-                        WrapPanel.itemWidth squareWidth
-                        WrapPanel.maxWidth (squareHeight * buttonsInColumn)
-                        
-                        WrapPanel.children[
-                          for i = 0 to state.GetLength(0) - 1 do
-                              for j = 0 to state.GetLength(1) - 1 do
-                                  createButton (ChangeCellState {Position.X = i; Position.Y = j})
-                            
-                                
-                          Button.create[
-                              Button.width 50.0
-                              Button.height 25.0
-                              Button.content("Start")
-                          ]
-                          Button.create[
-                              Button.margin 25.0
-                              Button.width 50.0
-                              Button.height 25.0
-                              Button.content("Stop")
-                          ]
-                          Button.create[
-                              Button.margin 50.0
-                              Button.width 50.0
-                              Button.height 25.0
-                              Button.content("Reset")
-                              Button.onClick(fun _ -> dispatch UserAction.Reset)
-                          ]
-                         
-                          
-                         
-                               
-                ]
-            ]
-                
-                
-                
-        ]
-              
-       ]
-          
-      
-            
-                
-                
-    
+
+        let createButton (cellCoordinates: UserAction) =
+            Button.create
+                [ Button.width squareWidth
+                  Button.height squareHeight
+                  Button.content (cellToString state cellCoordinates)
+                  Button.onClick (fun _ -> dispatch cellCoordinates)
+                  ]
+
+        DockPanel.create
+            [ DockPanel.children
+                  [ WrapPanel.create
+                        [ WrapPanel.horizontalAlignment HorizontalAlignment.Center
+                          WrapPanel.verticalAlignment VerticalAlignment.Center
+                          WrapPanel.dock Dock.Bottom
+                          WrapPanel.itemWidth squareWidth
+                          WrapPanel.maxWidth (squareHeight * buttonsInColumn)
+
+                          WrapPanel.children[for i = 0 to state.GetLength(0) - 1 do
+                                                 for j = 0 to state.GetLength(1) - 1 do
+                                                     createButton (ChangeCellState { Position.X = i; Position.Y = j })
+
+
+                                             Button.create
+                                                 [ Button.width 50.0; Button.height 25.0; Button.content "Start" ]
+
+                                             Button.create
+                                                 [ Button.margin 25.0
+                                                   Button.width 50.0
+                                                   Button.height 25.0
+                                                   Button.content "Stop" ]
+
+                                             Button.create
+                                                 [ Button.margin 50.0
+                                                   Button.width 50.0
+                                                   Button.height 25.0
+                                                   Button.content "Reset"
+                                                   Button.onClick (fun _ -> dispatch UserAction.Reset) ]
+
+
+
+
+                                             ] ]
+
+
+
+                    ]
+
+              ]
