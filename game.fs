d4a8b245de29f7bb71f57e124189c6fb46f53919
Vestue <61783376+Vestue@users.noreply.github.com>
2023-02-25 14:26:30 +0100

    Fix formatting

--- game.fs
+++ game.fs
@@ -12,7 +12,7 @@ open Microsoft.VisualBasic.CompilerServices
 module Game =
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
-    
+
     open Microsoft.FSharp.Collections
 
     type Cell =
@@ -52,7 +52,7 @@ module Game =
 
     let flipCellState (coordinates: Position) (state: State) : State =
         let newState: Cell[,] =
-            Array2D.init gridHeight gridWidth (fun x y ->
+            Array2D.init gridWidth gridHeight (fun x y ->
                 if x = coordinates.X && y = coordinates.Y then
                     match state[coordinates.X, coordinates.Y] with
                     | Alive -> Cell.Dead
@@ -61,38 +61,39 @@ module Game =
                     state[x, y])
 
         newState
-    
-    
-    let getNeighbours (state : State)(xCoord : int)(yCoord : int) : Cell list =
-        let rowRange = {max 0 (xCoord-1) .. min (gridWidth-1) (xCoord+1)}
-        let colRange = {max 0 (yCoord-1) .. min (gridHeight-1) (yCoord+1)}
-        [for row in rowRange do
-             for col in colRange do
-                 if row <> xCoord || col <> yCoord then yield state.[row,col]
-                 ]
-    let getLiveNeighbours (state : State)(xCoord: int)(yCoord: int) : int =
-            let neighbours = getNeighbours state xCoord yCoord
-            let rec loop neighbours amount = 
-                match neighbours with
-                |[] -> amount
-                |h::t ->
-                     match h with
-                     | Alive -> loop t amount+1
-                     | Dead -> loop t amount
-            loop neighbours 0
-   
-            
-        
-        
-    let generateNextGeneration (state : State) : State =
+
+    let getNeighbours (state: State) (xCoord: int) (yCoord: int) : Cell list =
+        let rowRange = { max 0 (xCoord - 1) .. min (gridWidth - 1) (xCoord + 1) }
+        let colRange = { max 0 (yCoord - 1) .. min (gridHeight - 1) (yCoord + 1) }
+
+        [ for row in rowRange do
+              for col in colRange do
+                  if row <> xCoord || col <> yCoord then
+                      yield state[row, col] ]
+
+    let getLiveNeighbours (state: State) (xCoord: int) (yCoord: int) : int =
+        let neighbours = getNeighbours state xCoord yCoord
+
+        let rec loop neighbours amount =
+            match neighbours with
+            | [] -> amount
+            | h :: t ->
+                match h with
+                | Alive -> loop t amount + 1
+                | Dead -> loop t amount
+
+        loop neighbours 0
+
+    let generateNextGeneration (state: State) : State =
         let newGen: Cell[,] =
-            Array2D.init gridHeight gridWidth (fun x y ->
-                match getLiveNeighbours state x y, state[x,y] with
+            Array2D.init gridWidth gridHeight (fun x y ->
+                match getLiveNeighbours state x y, state[x, y] with
                 | x, Dead when x = 3 -> Alive
                 | x, Alive when x = 3 || x = 2 -> Alive
                 | _, _ -> Dead)
+
         newGen
-        
+
     let rec update (msg: UserAction) (state: State) : State =
         match msg with
         | Start -> generateNextGeneration state
@@ -101,19 +102,21 @@ module Game =
         | ChangeCellState pos -> flipCellState pos state
         | _ -> state
 
+    let squareWidth = 25.0
+    let squareHeight = 25.0
+    let buttonsInColumn = 16.0
+
+
     let view (state: State) dispatch =
         printfn $"{state}"
-        let squareWidth = 25.0
-        let squareHeight = 25.0
-        let buttonsInColumn = 16.0
 
         let createButton (cellCoordinates: UserAction) =
             Button.create
                 [ Button.width squareWidth
                   Button.height squareHeight
                   Button.content (cellToString state cellCoordinates)
-                  Button.onClick (fun _ -> dispatch cellCoordinates)
-                  ]
+                  Button.onClick (fun _ -> dispatch cellCoordinates) ]
+
 
         DockPanel.create
             [ DockPanel.children
@@ -133,7 +136,7 @@ module Game =
                                                  [ Button.width 50.0
                                                    Button.height 25.0
                                                    Button.content "Start"
-                                                   Button.onClick(fun _ -> dispatch UserAction.Start) ]
+                                                   Button.onClick (fun _ -> dispatch UserAction.Start) ]
 
                                              Button.create
                                                  [ Button.margin 25.0
@@ -146,15 +149,4 @@ module Game =
                                                    Button.width 50.0
                                                    Button.height 25.0
                                                    Button.content "Reset"
-                                                   Button.onClick (fun _ -> dispatch UserAction.Reset) ]
-
-
-
-
-                                             ] ]
-
-
-
-                    ]
-
-              ]
+                                                   Button.onClick (fun _ -> dispatch UserAction.Reset) ]] ] ] ]
