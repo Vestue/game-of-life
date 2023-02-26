25920349d8a739ebd7a0bbe9b9c0225a0ab27efa
Vestue <61783376+Vestue@users.noreply.github.com>
2023-02-26 12:36:12 +0100

    Add working timer

--- game.fs
+++ game.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Linq.Expressions
+open System.Timers
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Layout
@@ -24,6 +25,7 @@ module Game =
     type UserAction =
         | ChangeCellState of Position
         | Start
+        | Run
         | Stop
         | Save
         | Load
@@ -32,16 +34,15 @@ module Game =
     type GridState =
         | Running
         | Stopped
+        
+    type Grid = Cell[,]
 
-    type State = Cell[,]
+    type State = { grid: Grid; timer: Timer}
 
-    let cellToString (state: State) (coords: UserAction) =
-        match coords with
-        | ChangeCellState pos ->
-            match state[pos.X, pos.Y] with
-            | Dead -> " "
-            | Alive -> "■"
-        | _ -> ""
+    let cellToString (cell: Cell) =
+        match cell with
+        | Alive -> "■"
+        | Dead -> " "
 
     let isCellAlive (cell: Cell) =
         match cell with
@@ -49,59 +50,72 @@ module Game =
         | Alive -> true
 
     let gridLength = 16
+    
+    let initTimer =
+        let timer = new Timer(1000.0)
+        timer.AutoReset <- true
+        timer
 
-    let init: State = Array2D.create gridLength gridLength Cell.Dead
+    let init: State = {grid = Array2D.create gridLength gridLength Cell.Dead; timer = initTimer}
 
     let flipCellState (coordinates: Position) (state: State) : State =
-        let newState: Cell[,] =
+        let newGrid: Cell[,] =
             Array2D.init gridLength gridLength (fun x y ->
                 if x = coordinates.X && y = coordinates.Y then
-                    match state[coordinates.X, coordinates.Y] with
+                    match state.grid[coordinates.X, coordinates.Y] with
                     | Alive -> Cell.Dead
                     | Dead -> Cell.Alive
                 else
-                    state[x, y])
+                    state.grid[x, y])
+        {grid = newGrid; timer = state.timer}
 
-        newState
-
-    let getNeighbours (state: State) (xCoord: int) (yCoord: int) : Cell list =
+    let getNeighbours (grid: Grid) (xCoord: int) (yCoord: int) : Cell list =
+        // Use min and max to prevent cells at the edges from attempting
+        // to access indexes that are not in the array.
         let rowRange = { max 0 (xCoord - 1) .. min (gridLength - 1) (xCoord + 1) }
         let colRange = { max 0 (yCoord - 1) .. min (gridLength - 1) (yCoord + 1) }
 
         [ for row in rowRange do
               for col in colRange do
                   if row <> xCoord || col <> yCoord then
-                      yield state[row, col] ]
+                      yield grid[row, col] ]
 
     let countAlive (cell: Cell) : int =
         match cell with
         | Alive -> 1
         | Dead -> 0
         
-    let sumLivingNeighbours (state: State) x y : int =
-        getNeighbours state x y
+    let sumLivingNeighbours (grid: Grid) x y : int =
+        getNeighbours grid x y
         |> List.fold (fun acc cell -> acc + countAlive cell) 0
 
     let generateNextGeneration (state: State) : State =
         let newGen: Cell[,] =
             Array2D.init gridLength gridLength (fun x y ->
-                match sumLivingNeighbours state x y, state[x, y] with
+                match sumLivingNeighbours state.grid x y, state.grid[x, y] with
                 | x, Dead when x = 3 -> Alive
                 | x, Alive when x = 3 || x = 2 -> Alive
                 | _, _ -> Dead)
-
-        newGen
-
-    let rec update (msg: UserAction) (state: State) : State =
-        match msg with
-        | Start -> generateNextGeneration state
-        | Stop -> state
-        | Reset -> init
+        {grid = newGen; timer = state.timer}
+        
+    let update (action: UserAction) (state: State) : State =
+        match action with
+        | Start ->
+            state.timer.Start()
+            state
+        | Stop ->
+            state.timer.Stop()
+            state
+        | Run -> generateNextGeneration state
+        | Reset ->
+            state.timer.Stop()
+            init
         | ChangeCellState pos -> flipCellState pos state
         | _ -> state
+        
 
     let view (state: State) dispatch =
-        printfn $"{state}"
+        state.timer.Elapsed.Add (fun _ -> dispatch Run)
 
         // Sizes for UI
         let squareLength = 30.0
@@ -109,12 +123,12 @@ module Game =
         let optionHeight = 30.0
         let optionWidth = 60.0
         
-        let createButton (cellCoordinates: UserAction) =
+        let createCellButton (pos: Position) =
             Button.create
                 [ Button.width squareLength
                   Button.height squareLength
-                  Button.content (cellToString state cellCoordinates)
-                  Button.onClick (fun _ -> dispatch cellCoordinates) ]
+                  Button.content (cellToString state.grid[pos.X, pos.Y])
+                  Button.onClick (fun _ -> dispatch (ChangeCellState pos)) ]
 
 
         DockPanel.create
@@ -126,25 +140,26 @@ module Game =
                           WrapPanel.itemWidth squareLength
                           WrapPanel.maxWidth (squareLength * buttonsInColumn)
 
-                          WrapPanel.children[for i = 0 to state.GetLength(0) - 1 do
-                                                 for j = 0 to state.GetLength(1) - 1 do
-                                                     createButton (ChangeCellState { Position.X = i; Position.Y = j })
+                          WrapPanel.children[for i = 0 to gridLength - 1 do
+                                                 for j = 0 to gridLength - 1 do
+                                                     createCellButton { X = i; Y = j }
 
                                              Button.create
                                                  [ Button.width optionWidth
                                                    Button.height optionHeight
                                                    Button.content "Start"
-                                                   Button.onClick (fun _ -> dispatch UserAction.Start) ]
+                                                   Button.onClick (fun _ -> dispatch Start) ]
 
                                              Button.create
                                                  [ Button.margin 35
                                                    Button.width optionWidth
                                                    Button.height optionHeight
-                                                   Button.content "Stop" ]
+                                                   Button.content "Stop" 
+                                                   Button.onClick (fun _ -> dispatch Stop) ]
 
                                              Button.create
                                                  [ Button.margin 70
                                                    Button.width optionWidth
                                                    Button.height optionHeight
                                                    Button.content "Reset"
-                                                   Button.onClick (fun _ -> dispatch UserAction.Reset) ]] ] ] ]
+                                                   Button.onClick (fun _ -> dispatch Reset) ]] ] ] ]
